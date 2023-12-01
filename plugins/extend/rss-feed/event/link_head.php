928a4e0276553785fe061e57f9dda0f4ecbcfdea
Jirka Daněk <jirka.danek@jdanek.eu>
2023-12-01 20:53:07 +0100

    Update link_head.php

--- plugins/extend/rss-feed/event/link_head.php
+++ plugins/extend/rss-feed/event/link_head.php
@@ -1,5 +1,7 @@
 <?php
 
+use Sunlight\Router;
+
 return function(array $args) {
-    $args['output'] .= "\n<link rel=\"alternate\" type=\"application/rss+xml\" href=\"" . Sunlight\Router::path("rss", ['absolute' => true]) . "\" title=\"" . _lang('rss-feed.generate.title') . "\">";
+    $args['output'] .= "\n<link rel=\"alternate\" type=\"application/rss+xml\" href=\"" . _e(Router::path("rss", ['absolute' => true])) . "\" title=\"" . _lang('rss-feed.generate.title') . "\">";
 };
